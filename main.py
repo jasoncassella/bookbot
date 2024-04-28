4be83eb2bfdcd19762cf46541185c5d0cd0e00d7
jasoncassella <jasoncassella1@gmail.com>
2024-04-28 06:23:55 -0400

    one day i will sort this list of dictionaries

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    book_path = 'books/frankenstein.txt'
+
+    with open(book_path) as f:
         text = f.read()
     
     word_count = get_word_count(text)
     letter_count = get_letter_count(text)
 
-    print(f'{word_count} words were found in the document')
-    print(letter_count)
+    letter_list = []
+
+    for letter, count in letter_count.items():
+        if letter.isalpha():
+            dict = {}
+            dict[letter] = count
+            letter_list.append(dict)
     
+    print(f"--- Begin report of {book_path}")
+    print(f'{word_count} words were found in the document\n')
+
+    for dict in letter_list:
+        for letter, value in dict.items():
+            print(f'The letter {letter} was found: {value} times')
+
 def get_word_count(text):
     words = text.split()
     return (len(words))
